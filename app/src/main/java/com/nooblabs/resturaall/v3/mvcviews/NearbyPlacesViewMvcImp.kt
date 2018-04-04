a2c51ec87b919a09b16a8da7a475b80bba4fb02d
Sourabh S. Rawat <dragneelfps@gmail.com>
2018-04-04 20:41:53 +0530

    Add method to update the data adapter of Recycler View

--- app/src/main/java/com/nooblabs/resturaall/v3/mvcviews/NearbyPlacesViewMvcImp.kt
+++ app/src/main/java/com/nooblabs/resturaall/v3/mvcviews/NearbyPlacesViewMvcImp.kt
@@ -3,6 +3,8 @@ package com.nooblabs.resturaall.v3.mvcviews
 import android.content.Context
 import android.os.Bundle
 import android.app.FragmentManager
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,9 +17,10 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
 import com.nooblabs.resturaall.R
 import com.nooblabs.resturaall.logd
+import com.nooblabs.resturaall.v3.controllers.RestaurantListAdapter
 import com.nooblabs.resturaall.v3.mvcviews.base.NearbyPlacesViewMvc
 import kotlinx.android.synthetic.main.abs_slidingup_layout.view.*
-import kotlinx.android.synthetic.main.nearby_locations_slidingup_layout.view.*
+import kotlinx.android.synthetic.main.nearby_places_slideup_content_layout.view.*
 
 class NearbyPlacesViewMvcImp(context: Context, layoutInflater: LayoutInflater, container: ViewGroup?) : NearbyPlacesViewMvc {
 
@@ -25,6 +28,7 @@ class NearbyPlacesViewMvcImp(context: Context, layoutInflater: LayoutInflater, c
     private var mLocationSelectedListener: NearbyPlacesViewMvc.LocationSelectedListener? = null
     private var mRootView: View
     private var mSearchBox : PlaceAutocompleteFragment? = null
+    private var adapter: RestaurantListAdapter? = null
     init {
         mRootView = layoutInflater.inflate(R.layout.abs_slidingup_layout, container)
         mRootView.main_content.addView(layoutInflater.inflate(R.layout.activity_nearby_places2, null))
@@ -53,9 +57,7 @@ class NearbyPlacesViewMvcImp(context: Context, layoutInflater: LayoutInflater, c
     }
 
     fun onLocationSelected(place: Place?){
-        mMap?.let {mMap ->
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place?.latLng, 10f))
-        }
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(place?.latLng, 10f))
     }
 
 
@@ -87,5 +89,11 @@ class NearbyPlacesViewMvcImp(context: Context, layoutInflater: LayoutInflater, c
 
     override fun getViewState(): Bundle? {
         return null
+    }
+
+    override fun updateAdapter(adapter: RestaurantListAdapter, context: Context) {
+        Log.d("app","Updating adapter")
+        mRootView.nearby_restaurant_list.layoutManager = LinearLayoutManager(context)
+        mRootView.nearby_restaurant_list.adapter = adapter
     }
 }
--- app/src/main/java/com/nooblabs/resturaall/v3/mvcviews/base/NearbyPlacesViewMvc.kt
+++ app/src/main/java/com/nooblabs/resturaall/v3/mvcviews/base/NearbyPlacesViewMvc.kt
@@ -1,15 +1,19 @@
 package com.nooblabs.resturaall.v3.mvcviews.base
 
 import android.app.FragmentManager
+import android.content.Context
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.places.Place
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.nooblabs.resturaall.v3.controllers.AwesomeActivity
+import com.nooblabs.resturaall.v3.controllers.RestaurantListAdapter
 
 interface NearbyPlacesViewMvc: MvcView, OnMapReadyCallback {
 
     fun setUpWidgets(fragmentManager: FragmentManager, locationSelectedListener: LocationSelectedListener?)
 
     fun setLocationSelectedListener(listener: LocationSelectedListener?)
+    fun updateAdapter(adapter: RestaurantListAdapter, context: Context)
 
     interface LocationSelectedListener{
         fun onLocationSelected(place: Place?)
